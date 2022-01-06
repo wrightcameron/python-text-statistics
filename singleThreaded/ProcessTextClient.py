d56116608200119cdfeebdb13844b6d55070bc09
Cameron Wright <ccameronwright@gmail.com>
2022-01-05 18:35:17 -0700

    Added the argument parser to the single threaded client and server.

--- singleThreaded/ProcessTextClient.py
+++ singleThreaded/ProcessTextClient.py
@@ -1,18 +1,21 @@
-from TextStatistics import TextStatistics
-
 import socket
 import argparse
+import sys
+
+sys.path.append('..')
+from TextStatistics import TextStatistics
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65442        # The port used by the server
 
-# parser = argparse.ArgumentParser(description='Get statistics on text.')
-# parser.add_argument('fileName', metavar='file', type=str, help='File name to parse')
-# parser.add_argument("-t", "--times", help="Run process x number of times", type=int, default=0)
-# #TODO Need flags for host and port
-# args = parser.parse_args()
-# fileName = args.fileName
-fileName = 'etext/testfile.txt'
+parser = argparse.ArgumentParser(description='Launch text statistics server.')
+parser.add_argument('fileName', metavar='file', type=str, help='File name to parse')
+parser.add_argument('-s', '--server', type=str, default=HOST,help='Server ip or host to launch server on, default is {}'.format(HOST))
+parser.add_argument('-p', '--port', type=str, default=PORT, help='Port to launch server on, default is {}'.format(PORT))
+args = parser.parse_args()
+host = args.server
+port = args.port
+fileName = args.fileName
 
 data = ""
 with open(fileName, 'r') as file:
--- singleThreaded/ProcessTextServer.py
+++ singleThreaded/ProcessTextServer.py
@@ -1,17 +1,23 @@
 import socket
 import argparse
+import sys
 
+sys.path.append('..')
 from TextStatistics import TextStatistics
-
-# parser = argparse.ArgumentParser(description='Launch text statistics server.')
-# parser.add_argument('Host', metavar='host', type=str, help='Host name to launch server on')
-# parser.add_argument('Port', metavar='port', type=str, help='Port to launch server on')
 
 HOST = '127.0.0.1'
 PORT = 65442
 
+parser = argparse.ArgumentParser(description='Launch text statistics server.')
+parser.add_argument('-s', '--server', type=str, default=HOST,help='Server ip or host to launch server on, default is {}'.format(HOST))
+parser.add_argument('-p', '--port', type=str, default=PORT, help='Port to launch server on, default is {}'.format(PORT))
+args = parser.parse_args()
+host = args.server
+port = args.port
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((host, port))
     s.listen()
     conn, addr = s.accept()
     with conn:
