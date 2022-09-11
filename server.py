4b2cf6cab058e835c220c1753ca60436ecbb931a
Cameron Wright <ccameronwright@gmail.com>
2022-09-11 13:21:46 -0600

    feature/1-sockets: Added handling to server so it gives up socket and port better if it exists.

--- server.py
+++ server.py
@@ -8,48 +8,45 @@ PORT = 65432
 
 
 def run(host: str, port: int):
-    # TODO Check if this can be captured in a with statement
-    # TODO Setup this server so it can exit gracefully, so the OS opens up the port
     # Create a TCP/IP socket.
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Have OS reuse socket address if process dies right away
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # Bind the socket to the port.
+        server_address = (host, port)
+        print('starting up on {} port {}'.format(*server_address))
+        sock.bind(server_address)
 
-    # Bind the socket to the port.
-    server_address = (host, port)
-    print('starting up on {} port {}'.format(*server_address))
+        # Listen for incoming connections.
+        sock.listen(1)
 
-    sock.bind(server_address)
-
-    # Listen for incoming connections.
-    sock.listen(1)
-
-    while True:
-        # Wait for a connection.
-        print('waiting for a connection')
-        conn, client_address = sock.accept()
-        try:
-            print('connection from', client_address)
-            with tempfile.TemporaryFile() as tempFile:
-                while True:
-                    # New
-                    msg = conn.recv(1024)
-                    # If message length is less than 1024, it could be last line
-                    if not msg or len(msg) < 1024:
-                        if msg:
-                            tempFile.write(msg)
-                        # Hit end of line
-                        break
-                    tempFile.write(msg)
-                tempFile.seek(0)
-                # File recieved, now need to pass files to ProcessText
-                contents = tempFile.read().decode('utf-8')
-                print(f"Contents of file are {contents}")
-                response = processText(contents)
-                if response:
-                    conn.sendall(response.encode('utf-8'))
-        finally:
-            # Clean up the connection.
-            conn.close()
-        print("Finished")
+        while True:
+            # Wait for a connection.
+            print('waiting for a connection')
+            conn, client_address = sock.accept()
+            try:
+                print('connection from', client_address)
+                with tempfile.TemporaryFile() as tempFile:
+                    while True:
+                        msg = conn.recv(1024)
+                        # If message length is less than 1024, it could be last line
+                        if not msg or len(msg) < 1024:
+                            if msg:
+                                tempFile.write(msg)
+                            # Hit end of line
+                            break
+                        tempFile.write(msg)
+                    tempFile.seek(0)
+                    # File recieved, now need to pass files to ProcessText
+                    contents = tempFile.read().decode('utf-8')
+                    print(f"Contents of file are {contents}")
+                    response = processText(contents)
+                    if response:
+                        conn.sendall(response.encode('utf-8'))
+            finally:
+                # Clean up the connection.
+                conn.close()
+            print("Finished")
 
 
 def processText(text: str):
