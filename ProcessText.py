effef932a420be128dc397762b6971ee17f45169
Cameron Wright <ccameronwright@gmail.com>
2022-09-11 12:33:21 -0600

    feature/1-sockets: Added single socket server and client, passing file to server from client.

--- ProcessText.py
+++ ProcessText.py
@@ -10,9 +10,9 @@ def runTextStatistics(files: list):
         files (list): List of all files
     """
     for f in files:
-         w = TextStatistics()
-         w.run(f)
-         print(w)
+        w = TextStatistics()
+        w.run(f)
+        print(w)
 
 # Entry point file for running Text Statistics from Command Line
 if __name__ == "__main__":
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     parser.add_argument('fileName', metavar='file', type=str, nargs='+', help='File name to parse')
     parser.add_argument("-t", "--times", help="Run process x number of times", type=int, default=0)
     args = parser.parse_args()
-    
+
     times = args.times
     print(times)
     # Running it n times, is to see performance
--- client.py
+++ client.py
@@ -1,42 +1,40 @@
 import socket
-import argparse
 import os
+import argparse
 
 HOST = "127.0.0.1"
 PORT = 65432
+
 
 def run(host: str, port: int, fileName: str):
     # Check if file exists
     if not os.path.isfile(fileName):
         print(f"File {fileName} doesn't exist. Exiting...")
         exit(1)
-    # Open file and store it in variable
-    with open(fileName) as f:
-        text = f.read()
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
-        s.connect((host, port))
+    # Use with statement New
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+        sock.settimeout(5)
         # Send text file to server
-        s.sendall(text.encode('utf-8'))
-        while True:
-            # TODO Send to the server the file path to the file it will analyze
-            msg = s.recv(1024)
+        print(f"Sending file {fileName}")
+        file = open(fileName, "rb")
+        l = file.read(1024)
+        while (l):
+            sock.send(l)
+            l = file.read(1024)
+        print("Finished sending file to host")
 
-            if not msg:
-                print('No messages from the server. Closing the connection...')
-                break
+        # Look for the response.
+        msg = sock.recv(1024).decode('utf-8')
+        print(msg)
 
-            print(f"Message from server: { msg.decode('utf-8') }")
-            print(f"Type of recieved message: {type(msg)}")
 
-run(HOST, PORT, './etext/testfile.txt')
-# TODO Uncomment when closer to completion
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description='ProcessTextClient: Get test statistics from server')
-#     parser.add_argument('-h', '--host', type=str, help='Server host/ip to connect too', default=HOST)
-#     parser.add_argument('-p', '--port', type=int, help='Port number', default=PORT)
-#     parser.add_argument('fileName', metavar='file', type=str, nargs='+', help='File name to parse')
-#     args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='ProcessTextClient: Get test statistics from server')
+    parser.add_argument('-s', '--host', type=str, help='Server host/ip to connect too', default=HOST)
+    parser.add_argument('-p', '--port', type=int, help='Port number', default=PORT)
+    parser.add_argument('fileName', metavar='file', type=str, help='File name to parse')
+    args = parser.parse_args()
 
-#     run(args.host, args.port, args.fileName)
+    run(args.host, args.port, args.fileName)
